be879395a90f4e019e3fca61e2806dea7775ecff
farahzaghdane <zaghdanefarah@gmail.com>
2026-03-15 06:34:12 -0400

    Fix CustomUser import typo and add AUTH_USER_MODEL to settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -129,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.CustomUser'
+
+
